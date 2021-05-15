8a149f7429be42042add834ef8fae2f4f99b67fc
Yven42 <55532134+Yven42@users.noreply.github.com>
2021-05-15 13:30:44 +0200

    first commit

---
+++ Core/Inc/Connection.h
@@ -0,0 +1,15 @@
+/*
+ * Connection.h
+ *
+ *  Created on: May 15, 2021
+ *      Author: Room
+ */
+
+#ifndef INC_CONNECTION_H_
+#define INC_CONNECTION_H_
+
+
+
+
+
+#endif /* INC_CONNECTION_H_ */
---
+++ Core/Src/Connection.cpp
@@ -0,0 +1,10 @@
+/*
+ * Connection.cpp
+ *
+ *  Created on: May 15, 2021
+ *      Author: Room
+ */
+
+
+
+
